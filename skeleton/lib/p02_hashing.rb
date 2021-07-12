ee45e175b4ca1f7e03f26f369aabc45437c9bee9
cgradeff <64032507+cgradeff@users.noreply.github.com>
2021-07-12 15:43:23 -0400

    p02 finished

--- skeleton/lib/p02_hashing.rb
+++ skeleton/lib/p02_hashing.rb
@@ -21,13 +21,41 @@ end
 
 class String
   def hash
+    alph = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a
+    str = ''
+    self.split('').each do |ele|
+      if alph.index(ele) > 9
+        str += alph.index(ele).to_s
+      else
+        str += ('0' + alph.index(ele).to_s)
+      end
+    end
+    str.to_i
   end
 end
 
-class Hash
+class Hash 
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
     0
+    new_hash = self.sort
+    key_arr = new_hash
+    key_str = ''
+    val_str = ''
+    str_to_i(key_arr.join('')) 
+  end
+
+  def str_to_i(string)
+    alph = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a
+    str = ''
+    string.split('').each do |ele|
+      if alph.index(ele) > 9
+        str += alph.index(ele).to_s
+      else
+        str += ('0' + alph.index(ele).to_s)
+      end
+    end
+    str.to_i
   end
 end
